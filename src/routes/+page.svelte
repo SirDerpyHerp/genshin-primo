ceb90dd117275d486c188590c6491ed85bf71449
Derp <Derp>
2024-03-15 15:50:24 +0700

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <title>Genshin Primo Planner</title>
+</svelte:head>
+
+<header class='flex justify-center items-center'>
+    <h1 class='h-full text-2vh'>Genshin Primo Planner</h1>
+</header>
